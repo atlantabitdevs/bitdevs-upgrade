25789f8582019b710a2d050e4245be2594412df7
Jordan Bravo <jordan@bravo.cc>
2023-07-22 13:44:41 -0400

    Create some test pages

--- app/events/first-event/page.tsx
+++ app/events/first-event/page.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link'
 
-export default function Home() {
+export default function FirstEvent() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>This is the home page, bruh</h1>
+      <code>/events/first-event</code>
+      <h1>This is the FirstEvent page, bruh</h1>
+      <Link href="/">Home</Link>
       <Link href="/events">Events</Link>
     </main>
   )
