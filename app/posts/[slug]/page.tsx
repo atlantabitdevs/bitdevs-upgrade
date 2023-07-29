a7310d83758a10256d9620b32731cdb57d80568d
Stephen DeLorme <stephen@d.elor.me>
2023-07-29 11:47:29 -0400

    fixed possibly undefined post

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -21,6 +21,10 @@ async function getDocFromParams(slug: string) {
 const page = async ({ params }: PageProps) => {
     const post = await getDocFromParams(params.slug)
     
+    if(!post) {
+        return <div>404 sorry you poor bitdev</div>
+    }
+    
     console.log(post._raw.flattenedPath)
 
     // return <div>{JSON.stringify(post)}</div>
