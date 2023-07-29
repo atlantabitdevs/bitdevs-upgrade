9a218195cf0235800aaf097c2f68b8589621a3e3
Nitesh Balusu <84944042+niteshbalusu11@users.noreply.github.com>
2023-07-29 18:01:30 -0400

    add styling to summaries

--- components/SummaryLink.tsx
+++ components/SummaryLink.tsx
@@ -9,17 +9,31 @@ type Props = {
 
 export default function SummaryLink(props: Props) {
   // TODO: Fetch the summary from the JSON file corresponding to this link using the props.slug
-  // TODO: Figure out how to prevent this from being housed in a <p> tag when rendered, I want to use more semantic HTML intside this component
+  // TODO: Figure out how to prevent this from being housed in a <p> tag when rendered, I want to use more semantic HTML inside this component
 
   return (
-    <div>
+    <div className="bg-dark text-white p-4 space-y-4">
+      {' '}
+      {/* Added dark background, white text, padding, and vertical spacing */}
       {!!props.data
         ? props.data.summary.map((n) => (
-            <>
-              <h1>{n.title}</h1>
-              <p>{n.summary}</p>
-              <p>{n.summaryeli15}</p>
-            </>
+            <div key={n.title} className="space-y-2">
+              {' '}
+              {/* Added vertical spacing and unique key */}
+              <h1 className="text-3xl font-bold">{n.title}</h1>{' '}
+              {/* Added text size and bold font */}
+              <div className="pl-2">
+                {' '}
+                {/* Added padding to the left */}
+                <p className="text-lg">{n.summary}</p> {/* Added text size */}
+              </div>
+              <div className="pl-2">
+                {' '}
+                {/* Added padding to the left */}
+                <p className="text-sm">{n.summaryeli15}</p>{' '}
+                {/* Added text size */}
+              </div>
+            </div>
           ))
         : null}
     </div>
