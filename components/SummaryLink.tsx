5c59fdb41829234fae40df0ab87ed213b521b003
Stephen DeLorme <stephen@d.elor.me>
2023-07-29 15:28:49 -0400

    Passing slug to summary link

--- components/SummaryLink.tsx
+++ components/SummaryLink.tsx
@@ -1,9 +1,9 @@
 export default function SummaryLink(props:any) {
-    // Fetch the summary from the JSON file corresponding to this link
+    // TODO: Fetch the summary from the JSON file corresponding to this link using the props.slug
     
     return(
         <div>
-            <h3><a href={props.href}>{props.title}</a></h3>
+            <h3><a href={props.href}>{props.title}</a> {props.slug}</h3>
             <h4>Cliff Notes</h4>
             <p>Cliff summary goes here</p>
             <h4>ELI5</h4>
