935ff120409564717d5470bc46458726b052a511
Stephen DeLorme <stephen@d.elor.me>
2023-07-29 17:22:36 -0400

    added events and posts to home

--- app/[contentType]/page.tsx
+++ app/[contentType]/page.tsx
@@ -1,8 +1,5 @@
 import Link from 'next/link'
-import {
-  ContentType,
-  getSortedMarkdownContent,
-} from '../../lib/parse-markdown-files'
+import { getSortedMarkdownContent } from '../../lib/parse-markdown-files'
 import MeetupName from '@/components/MeetupName'
 
 export default function Posts({ params }: { params: any }) {
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,44 @@
 import Link from 'next/link'
 import MeetupName from '@/components/MeetupName'
+import { getSortedMarkdownContent, ContentType } from '@/lib/parse-markdown-files'
 
 export default function Home({}) {
+  const eventsContentData = getSortedMarkdownContent(ContentType.Events)
+  const postsContentData = getSortedMarkdownContent(ContentType.Posts)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <MeetupName />
       <h1>This is the home page, bruh</h1>
       <hr />
-      <h2>The Events component goes here.</h2>
+      <h2>Events Post</h2>
+      <ul>
+        {eventsContentData.map(({ id, date, title }) => (
+          <li className="post" key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </li>
+        ))}
+      </ul>
+
       <Link href="/events" style={{ textDecoration: 'underline' }}>
         Click here to see all events...
       </Link>
-      <h2>The Posts component goes here.</h2>
+      <h2>Blog Posts</h2>
+      <ul>
+        {postsContentData.map(({ id, date, title }) => (
+          <li className="post" key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </li>
+        ))}
+      </ul>
       <Link href="/posts" style={{ textDecoration: 'underline' }}>
         Click here to see all posts...
       </Link>
