377f325b8b5ddb1e2bb433f4bc2419e38c513a54
Stephen DeLorme <stephen@d.elor.me>
2023-07-30 00:15:42 -0400

    placeholder text

--- metaupdata.ts
+++ metaupdata.ts
@@ -2,8 +2,8 @@ import { Metaupdata } from './types/Metaupdata'
 
 export const metaupdata: Metaupdata = {
   city: {
-    name: 'NYC',
-    position: 'right',
+    name: 'New',
+    position: 'left',
   },
   description:
     'BitDevs is a community for those interested in discussing and participating in the research and development of Bitcoin and related protocols',
