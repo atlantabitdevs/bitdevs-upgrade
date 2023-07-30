de3878cafacf135fd6590f1855982234cc54d738
Nitesh Balusu <84944042+niteshbalusu11@users.noreply.github.com>
2023-07-30 14:22:25 -0400

    ignore links with no summary

--- ai/src/index.ts
+++ ai/src/index.ts
@@ -48,39 +48,45 @@ const main = async () => {
         'getScrapedData',
         'initAi',
         async ({ getScrapedData, initAi }) => {
-          // const aiResults = async (summary: ScrapedDataResults) => {
-          //   try {
-          //     const aiCompletion = await Promise.all([
-          //       initAi.openai.createChatCompletion({
-          //         model,
-          //         messages: [
-          //           { role: 'user', content: `Can you summarize the text after the asterisk for me? Use no more than 160 words. Split into paragraphs where appropriate. * \n ${summary.text}` },
-          //         ],
-          //       }),
-          //       initAi.openai.createChatCompletion({
-          //         model,
-          //         messages: [
-          //           { role: 'user', content: `Can you summarize the text after the asterisk for me? Use no more than 100 words. Split into paragraphs where appropriate. Write it for a reader that may not be familiar with the technical jargon in the original text. Assume the reader is 15. * ${summary.text} ` },
-          //         ],
-          //       }),
-          //     ]);
-          //     const chatCompletion = aiCompletion[0].data.choices[0].message?.content || 'No summary generated';
-          //     const chatCompletionEli15 = aiCompletion[1].data.choices[0].message?.content || 'No summary generated';
-          //     return {
-          //       summary: chatCompletion,
-          //       summaryeli15: chatCompletionEli15,
-          //       title: summary.title,
-          //       link: summary.link,
-          //     } as SummaryResults;
-          //   } catch (err: any) {
-          //     console.error(`Summarization failed for ${summary.title} \n`);
-          //     console.log(err.response.data);
-          //   }
-          // };
-          // const results = (await mapLimit(getScrapedData.results, 3, aiResults)).filter(
-          //   (result): result is SummaryResults => result !== undefined
-          // );
-          // await writeSummary({ path: getScrapedData.summaryPath, data: results });
+          const aiResults = async (summary: ScrapedDataResults) => {
+            try {
+              const aiCompletion = await Promise.all([
+                initAi.openai.createChatCompletion({
+                  model,
+                  messages: [
+                    {
+                      role: 'user',
+                      content: `Can you summarize the text after the asterisk for me? Use no more than 160 words. Split into paragraphs where appropriate. * \n ${summary.text}`,
+                    },
+                  ],
+                }),
+                initAi.openai.createChatCompletion({
+                  model,
+                  messages: [
+                    {
+                      role: 'user',
+                      content: `Can you summarize the text after the asterisk for me? Use no more than 100 words. Split into paragraphs where appropriate. Write it for a reader that may not be familiar with the technical jargon in the original text. Assume the reader is 15. * ${summary.text} `,
+                    },
+                  ],
+                }),
+              ]);
+              const chatCompletion = aiCompletion[0].data.choices[0].message?.content || 'No summary generated';
+              const chatCompletionEli15 = aiCompletion[1].data.choices[0].message?.content || 'No summary generated';
+              return {
+                summary: chatCompletion,
+                summaryeli15: chatCompletionEli15,
+                title: summary.title,
+                link: summary.link,
+              } as SummaryResults;
+            } catch (err: any) {
+              console.error(`Summarization failed for ${summary.title} \n`);
+              console.log(err.response.data);
+            }
+          };
+          const results = (await mapLimit(getScrapedData.results, 3, aiResults)).filter(
+            (result): result is SummaryResults => result !== undefined
+          );
+          await writeSummary({ path: getScrapedData.summaryPath, data: results });
         },
       ],
     });
