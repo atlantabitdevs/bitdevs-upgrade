0d39d38f96ab0ba6b81252a9d85ee0b2f2191a5c
Stephen DeLorme <stephen@d.elor.me>
2023-07-31 19:28:15 -0400

    dark mode tweaks

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { SunIcon, MoonIcon } from '@bitcoin-design/bitcoin-icons-react/filled'
 
 import { useEffect, useState } from 'react'
 
@@ -18,11 +19,15 @@ export const ThemeSwitcher = () => {
 
   return (
     <button
-      className={`w-fit right-5 top-2 p-2 rounded-md hover:scale-110 active:scale-100 duration-200 bg-slate-200 dark:bg-[#212933]`}
+      className={`w-fit right-5 top-2 p-2 rounded-md hover:scale-110 active:scale-100 duration-200`}
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
       aria-label="Toggle Dark Mode"
     >
-      {theme === 'dark' ? '☀️' : '🌚'}
+      {theme === 'dark' ? (
+        <SunIcon className="w-6 h-6" />
+      ) : (
+        <MoonIcon className="w-6 h-6" />
+      )}
     </button>
   )
 }
