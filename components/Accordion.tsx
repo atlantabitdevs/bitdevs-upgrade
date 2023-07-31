b060b106ec8a3365f41f55c3de17ef229105d283
Stephen DeLorme <stephen@d.elor.me>
2023-07-31 11:30:15 -0400

    whoever wrote this ESLint rule needs to get a fucking life, for real

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -18,7 +18,7 @@ export default function Accordion(props:AccordionProps){
                 <span className="flex flex-row gap-2 cursor-pointer" onClick={()=>{console.log('test'); setExpanded(!expanded)}}>
                     <span>{props.type === "cliff" ? <>📄</> : <>🧒</>}</span>
                     <span className="text-xl font-sans font-semibold">
-                        {props.type === "cliff" ? <>Summarized</> : <>Explain Like I'm 5</>}
+                        {props.type === "cliff" ? <>Summarized</> : <>Explain Like I&rsquo;m 5</>}
                     </span>
                     <span>
                         {expanded ? <CaretUpIcon className="w-6 h-6" /> : <CaretDownIcon className="w-6 h-6" />}
