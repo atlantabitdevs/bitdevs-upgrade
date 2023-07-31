9f3d21e77beb1ae52e4e3d4a4e6b4868fdbbb397
bnonni <bryan@nonni.io>
2023-07-31 10:23:57 -0400

    MeetupName should be mobile friendly

--- components/MeetupName.tsx
+++ components/MeetupName.tsx
@@ -6,8 +6,16 @@ const {
 
 export default function MeetupName() {
   return (
-    <div className="flex flex-col space-y-1">
-      {position === 'right' ? <span>BitDevs <span className="meetup-name">{name}</span></span> : <span><span className="meetup-name">{name}</span> BitDevs</span>}
+    <div className="flex flex-col space-y-1 text-base">
+      {position === 'right' ? (
+        <span>
+          BitDevs <span className="meetup-name">{name}</span>
+        </span>
+      ) : (
+        <span>
+          <span className="meetup-name">{name}</span> BitDevs
+        </span>
+      )}
     </div>
   )
 }
