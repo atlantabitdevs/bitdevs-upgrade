753a694e17eee05324f6f18ae98bd8058d4639d6
Stephen DeLorme <stephen@d.elor.me>
2023-08-01 16:28:53 -0400

    Fix for #60

--- components/SummaryLink.tsx
+++ components/SummaryLink.tsx
@@ -1,5 +1,6 @@
 import { ParsedData } from '@/lib/get-json'
 import Accordion from './Accordion'
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline'
 
 type Props = {
   href: string | undefined
@@ -18,7 +19,14 @@ export default function SummaryLink(props: Props) {
     <span className="pt-4 mb-10 block">
       {!!props.data ? (
         <span key={props.title} className="flex flex-col gap-2">
-          <span className="text-xl font-sans font-semibold">{props.title}</span>
+          <a
+            href={props.href}
+            target="_blank"
+            className="text-xl font-sans font-semibold no-underline"
+          >
+            {props.title}
+            <ArrowTopRightOnSquareIcon className="w-6 h-6 inline-block ml-4 relative -top-1" />
+          </a>
           {entry ? (
             <>
               <Accordion content={entry?.summary} type="cliff" />
